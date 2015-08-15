4ce6fbbad7343255aee405b4706de4b86f77e140
Andrew Codispoti <andrewcod749@gmail.com>
2015-08-15 14:45:15 -0400

    Some logic to save the results in memory.

--- app.py
+++ app.py
@@ -1,22 +1,55 @@
-from flask import Flask
+from flask import Flask,request,render_template
 import requests
 from lxml import html
 import lxml
-#from urllib.parse import quote
 import time
 import pdb
-#from urllib.request import urlopen,Request
-
+import kat
 app = Flask(__name__)
+import json
 
+class SearchResult():
+    def __init__(self, name, magnet, link, size, seeders, leachers):
+        self.title = name
+        self.magnet = magnet
+        self.link = link
+        self.size = size
+        self.quality = seeders/leachers
+
+class ResultList():
+    def __init__(self, name):
+        self.name = name
+        self.results = []
+    def appendResult(self, result):
+        self.results.append(result)
+    def appendResults(self,results):
+        self.results += results
+
+
+searchResults = []
 @app.route('/')
 def serveMain():
     pass
 
+@app.route('/popular')
+def returnTop():
+    return kat.popular(category=kat.Categories.BOOKS)
+
 @app.route('/search')
 def searchBook():
-    return "Hello World"
+    query = request.args.get('q')
+
+    resultList = ResultList(query)
+
+    results = kat.search(query)
+
+    for x in results:
+        tempResult = SearchResult(x.title, x.magnet, x.download, x.size, x.seeders, x.leechers)
+        resultList.appendResult(tempResult)
+
+    searchResults.append(resultList)
+    return "Success"
 
 #Start the application
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
