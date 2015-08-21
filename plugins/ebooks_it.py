4b868807afd8abc4ce83b7907767da26360135c2
Andrew Codispoti <andrewcod749@gmail.com>
2015-08-20 23:20:50 -0400

    some code for ebooks it.

--- plugins/ebooks_it.py
+++ plugins/ebooks_it.py
@@ -1,1 +1,14 @@
-url = "http://"
+from imports import *
+baseurl = "https://www.ebooks-it.net/"
+url = baseurl + "search/?q=%s&type=title"
+def searchBook(query):
+    text = urllib2.urlopen(url%query).read()
+    page = html.fromstring(text)
+    listitems = page.xpath(".//h3/a")
+    for x in listitems:
+        title = x.xpath("./text()")
+        link = x.xpath("./@href")
+
+        newpage = html.fromstring(urllib2.urlopen(link).read())
+
+    print htmlstring
