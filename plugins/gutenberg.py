abdaf06324cffb12db675064ffd2515ea5d91793
Andrew Codispoti <andrewcod749@gmail.com>
2015-08-20 20:25:16 -0400

    Some parsing stuff.

--- plugins/gutenberg.py
+++ plugins/gutenberg.py
@@ -1,2 +1,12 @@
-url = "http://gutenberg.org"
-import lxml
+from imports import *
+url = "http://www.gutenberg.org/ebooks/search/?query="
+def searchBook(query):
+    text = urllib2.urlopen(url+query).read()
+    page = html.fromstring(text)
+    ulitems = page.xpath("//ul[@class = 'results']/li[@class = 'booklink']")
+    for x in ulitems:
+        print("\n\n\n")
+        title = x.xpath("//span[@class = 'title']/text()")
+        print title
+    pdb.set_trace()
+searchBook("sherlock")
---
+++ plugins/imports.py
@@ -0,0 +1,4 @@
+from lxml import etree
+from lxml import html
+import urllib2
+import pdb
