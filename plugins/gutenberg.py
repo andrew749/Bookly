cfd527b3c144d08e4ecb40b22039f895af628303
Andrew Codispoti <andrewcod749@gmail.com>
2015-08-20 23:11:34 -0400

    Fully working gutenberg.

--- plugins/gutenberg.py
+++ plugins/gutenberg.py
@@ -1,12 +1,16 @@
 from imports import *
-url = "http://www.gutenberg.org/ebooks/search/?query="
+baseurl = "http://www.gutenberg.org/"
+url = baseurl+"ebooks/search/?query="
 def searchBook(query):
     text = urllib2.urlopen(url+query).read()
     page = html.fromstring(text)
-    ulitems = page.xpath("//ul[@class = 'results']/li[@class = 'booklink']")
+    ulitems = page.xpath(".//li[@class='booklink']")
     for x in ulitems:
-        print("\n\n\n")
-        title = x.xpath("//span[@class = 'title']/text()")
-        print title
-    pdb.set_trace()
+        title = x.xpath(".//span[@class='title']/text()")[0]
+        author = x.xpath(".//span[@class='subtitle']/text()")[0]
+        link = x.xpath(".//a/@href")[0]
+
+        newpage = html.fromstring(urllib2.urlopen(baseurl+link).read())
+        li2 = newpage.xpath("//a[@class='link' and @type='application/epub+zip']/@href")[0]
+        print (title,author,li2[2:])
 searchBook("sherlock")
