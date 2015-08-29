b1d43d869cba7d3f0550ff4af8b40fdb675b2419
Andrew Codispoti <andrewcod749@gmail.com>
2015-08-28 22:27:47 -0400

    Commented download type.

--- SearchResult.py
+++ SearchResult.py
@@ -1,4 +1,5 @@
 import json
+#Type 0 is direct 1 is torrent.
 class SearchResult():
     def __init__(self, name, link, size=0, page=None, leachers=0, magnet=None, seeders=0, type=1):
         self.title = name
