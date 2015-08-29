c025ff01331cf0453bedad6f228efe4237a60543
Andrew Codispoti <andrewcod749@gmail.com>
2015-08-28 22:02:10 -0400

    Import search result object in plugins pages.

--- plugins/imports.py
+++ plugins/imports.py
@@ -2,3 +2,8 @@ from lxml import etree
 from lxml import html
 import urllib2
 import pdb
+import os,sys
+parentdir = os.path.dirname(__file__)
+sys.path.insert(0,parentdir)
+
+from SearchResult import SearchResult
