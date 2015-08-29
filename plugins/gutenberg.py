327a00694cb8008daf747f779a2fc4207a4042fe
Andrew Codispoti <andrewcod749@gmail.com>
2015-08-29 16:56:35 -0400

    More error handling with gutenberg.

--- plugins/gutenberg.py
+++ plugins/gutenberg.py
@@ -2,20 +2,26 @@ from imports import *
 baseurl = "http://www.gutenberg.org/"
 url = baseurl+"ebooks/search/?query="
 def searchBook(query):
-    text = urllib2.urlopen(url+query).read()
-    page = html.fromstring(text)
-    ulitems = page.xpath(".//li[@class='booklink']")
     searchResults = []
-    for x in ulitems:
-        try:
-            title = x.xpath(".//span[@class='title']/text()")[0]
-            author = x.xpath(".//span[@class='subtitle']/text()")[0]
-            link = x.xpath(".//a/@href")[0]
-            newpage = html.fromstring(urllib2.urlopen(baseurl+link).read())
-            li2 = newpage.xpath("//a[@class='link' and @type='application/epub+zip']/@href")[0]
-            finalLink = li2[2:]
-            tempResult = SearchResult(title, finalLink, type=0)
-            searchResults.append(tempResult)
-        except:
-            pass
+    try:
+        text = urllib2.urlopen(url+query).read()
+        page = html.fromstring(text)
+        ulitems = page.xpath(".//li[@class='booklink']")
+        count = 0
+        for x in ulitems:
+            if(count < 5):
+                count += 1
+                try:
+                    title = x.xpath(".//span[@class='title']/text()")[0]
+                    author = x.xpath(".//span[@class='subtitle']/text()")[0]
+                    link = x.xpath(".//a/@href")[0]
+                    newpage = html.fromstring(urllib2.urlopen(baseurl+link).read())
+                    li2 = newpage.xpath("//a[@class='link' and @type='application/epub+zip']/@href")[0]
+                    finalLink = li2[2:]
+                    tempResult = SearchResult(title, finalLink, type=0)
+                    searchResults.append(tempResult)
+                except:
+                    pass
+    except:
+        pass
     return searchResults
