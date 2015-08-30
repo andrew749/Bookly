ff8cc01b6905d1e46a33e0cb3bb2bf88029049f0
Andrew Codispoti <andrewcod749@gmail.com>
2015-08-30 15:31:24 -0400

    Added debugging statement to data.

--- ResultList.py
+++ ResultList.py
@@ -7,5 +7,8 @@ class ResultList():
         self.results.append(result)
     def appendResults(self,results):
         self.results += results
+    def printList(self):
+        for x in self.results:
+            print x
     def toJSON(self):
         return json.dumps([x.toJSON() for x in self.results], indent = 4)
